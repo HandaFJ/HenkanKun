13c0101ce9799f8b6860f170b7a86c7f07cd628b
Handa <yumemiruc@gmail.com>
2026-02-15 14:56:50 +0900

    初回デプロイ

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "npm:@vitejs/plugin-react@4.3.3";
 //import tailwindcss from "npm:@tailwindcss/vite@4.0.0";
 
 export default defineConfig({
+  base: "/HenkanKun/",
   plugins: [
     react(),
     //tailwindcss(),
